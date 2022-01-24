b22c01fc7604fc678ed0fdc5cf35b9f61248d8c5
KevvKo <kklein915@gmail.com>
2022-01-24 21:33:02 +0100

    add build tasks

---
+++ build/CMakeFiles/space-invader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/space-invader.dir/main.cpp.o"
+  "space-invader"
+  "space-invader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/space-invader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

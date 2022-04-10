5e0b3afabb907202af6005bdc821525cdc6dfae7
KevvKo <kevin.klein@geomagic.world>
2022-04-10 21:07:40 +0200

    small changes

--- build/CMakeFiles/space-invader.dir/cmake_clean.cmake
+++ build/CMakeFiles/space-invader.dir/cmake_clean.cmake
@@ -1,6 +1,8 @@
 file(REMOVE_RECURSE
   "CMakeFiles/space-invader.dir/main.cpp.o"
+  "CMakeFiles/space-invader.dir/main.cpp.o.d"
   "CMakeFiles/space-invader.dir/src/game.cpp.o"
+  "CMakeFiles/space-invader.dir/src/game.cpp.o.d"
   "space-invader"
   "space-invader.pdb"
 )
